27b36d497b1eb1556a44af104e0e18375ffa8b2f
VMedeiros <victor.leitao95@gmail.com>
2017-09-21 09:07:19 -0300

    Correções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, NavController, Nav } from 'ionic-angular';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
